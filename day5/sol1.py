0c467e7ef412a41a25ec0a3f38c65b1a2367d4c8
JensNys <jens.nys@student.kuleuven.be>
2022-12-05 11:25:29 +0100

    day 4

---
+++ day5/sol1.py
@@ -0,0 +1,61 @@
+
+
+
+def parse_action(line):
+    line = line[5:]
+
+    for i in range(len(line)):
+        if line[i]==" ":
+            amount = line[:i]
+
+            line = line[i + 6:]
+            break
+    for i in range(len(line)):
+        if line[i] == " ":
+            start = line[:i]
+
+            line = line[i+4:]
+            break
+    end = int(line)
+
+    return (int(amount),int(start),int(end))
+
+def stackify(stringstack):
+    r=[]
+    for i in range(10):
+        r.append([])
+    print(r)
+    for i in range(len(stringstack)-2):
+        for j in range(9):
+            if stringstack[i][1+4*j] != ' ':
+                r[j+1].append(stringstack[i][1+4*j])
+    return r
+    print(r)
+def main(args):
+    file = open(args, "r")
+    lines = file.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i][:-1]
+    stack = stackify(lines[:10])
+
+    print((lines[:10]))
+
+    print(stack)
+    for i in range(10,len(lines)):
+        (amount,start,finish) = parse_action(lines[i])
+        print((amount,start,finish))
+        buffer = stack[start][:amount]
+        stack[start]= stack[start][amount:]
+        while len(buffer) != 0:
+            stack[finish].insert(0,buffer[0])
+            buffer = buffer[1:]
+
+    length = 0
+    for line in stack:
+        print(line)
+
+
+
+
+if __name__ == '__main__':
+    main('input.txt')
